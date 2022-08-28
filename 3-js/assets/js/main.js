c26d71517d961831d0efa377d21ee1c867dc52ab
Gabriel <gabrielredu@gmail.com>
2022-08-28 14:30:19 -0300

    Finalizado o teste de JS

--- 3-js/assets/js/main.js
+++ 3-js/assets/js/main.js
@@ -5,6 +5,49 @@
 
 // Vanilla JS
 
-// window.onload = function() {
-  // code
-// };
+window.onload = function() {
+  const menu = document.querySelector('#menu')
+  const menuButton = document.querySelector('#menu-button');
+  const videoCover = document.querySelector('#video-cover');
+  const videoPlayer = document.querySelector('#video-player');
+  const accordionItem = document.querySelector('.item')
+  const wikipediaContent = document.querySelector('.extract');
+  const modalButton = document.querySelector('#button-modal');
+  const modal = document.querySelector('.modal-wiki');
+  const closeModal = document.querySelector('#close-modal');
+
+  function toggleClassActiveOnElement (element) {
+    element.classList.toggle('-active');
+  }
+
+  function addClassInactiveOnElement (element) {
+    element.classList.toggle('-inactive');
+  }
+
+  function openCloseModal () {
+    modal.classList.toggle('modal-wiki-active');
+  }
+
+  menuButton.addEventListener('click', () => {
+    toggleClassActiveOnElement(menu);
+  })
+
+  videoPlayer.addEventListener('click', () => {
+    addClassInactiveOnElement(videoCover);
+  })
+
+  accordionItem.addEventListener('click', () => {
+    toggleClassActiveOnElement(accordionItem);
+  })
+
+  fetch('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Alber%20Einstein&origin=*')
+    .then(response => response.json())
+    .then(json => {
+      const extract = json.query.pages[736].extract;
+      wikipediaContent.innerHTML = extract;
+    })
+
+  modalButton.addEventListener('click', openCloseModal);
+
+  closeModal.addEventListener('click', openCloseModal);
+};
